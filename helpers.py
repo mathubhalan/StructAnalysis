c5e696914b164f74ed4d4066d9e55e9188cd64c7
Mathu_Gopalan <mathu_gopalan@infosys.com>
2018-03-12 12:55:56 +0000

    Code optimized

--- helpers.py
+++ helpers.py
@@ -4,7 +4,8 @@ Created on Fri Mar  2 13:43:09 2018
 
 @author: Mathu_Gopalan
 """
-import glob, cv2, yaml, os, re
+import glob, cv2, yaml, os, re, time,pyautogui, util
+from selenium import webdriver
 
 
 def createcanny(inputpath,outputpath):
@@ -15,12 +16,14 @@ def createcanny(inputpath,outputpath):
         img = cv2.imread(imgpath)
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         edges = cv2.Canny(gray,50,150,apertureSize = 3)
-        cv2.imwrite(outputpath +"/"+ base +'_edg.jpg',edges)
+        op = outputpath +"/"+ base +'_edg.jpg'
+        cv2.imwrite(op,edges)
+        print ("Canny created: {}".format(op))
         i+=1
     print ("Canny Images completed in folder {}".format(outputpath))
     return True
 
-def ReadConfig(config_file):    
+def readconfig(config_file):    
     with open(config_file,'r') as ymlfile:
         cfg = yaml.load(ymlfile)
     for section in cfg:
@@ -43,7 +46,7 @@ def ReadConfig(config_file):
     print("Configurations completed")          
     return config
  
-def createFolders(fol_loc):
+def createfolders(fol_loc):
     del fol_loc['InputFile']
     for k,v in fol_loc.items():
         if not os.path.exists(v):
@@ -58,3 +61,35 @@ def slugify(title):
     name = re.sub('-+', '-', name)
     print(name)
     return name     
+
+def gen_screensnap(df,configs,method=None):
+    options = webdriver.ChromeOptions()
+    options.add_argument('--load-extension='+configs["ExtensionPath"])
+    # Navigate to any page... well, not just *any* page...
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.maximize_window()
+    driver.get("https://www.google.com")
+    time.sleep(2)
+    i=0
+    if (method=="wireframe"):
+        v=pyautogui.locateOnScreen("findicon.PNG")
+        folder_loc = configs["wfImages"]
+    else : folder_loc = configs["Rawimages"]
+    
+    for url,b_name in zip(df["urls_name"],df["brand_name"]):
+        driver.get(url.strip())
+        time.sleep(2)
+        pg_title = driver.title
+        pg_title = slugify(pg_title)
+        #pg_title = re.sub('[^a-zA-Z0-9-_!@#©|?|$|.|!*.]', '',pg_title)
+        pg_title = b_name+"_"+str(i)+ "_"+ pg_title
+        if len(pg_title)>240:
+            pg_title = pg_title[0:239]
+        print(pg_title)
+        if (method =="wireframe"):
+             #pyautogui to click on the extension
+            pyautogui.click(x=v[0],y=v[1],clicks=1,interval=0.0,button="left")
+            time.sleep(5)
+        util.fullpage_screenshot(driver,folder_loc,pg_title)
+        i+=1
+    driver.close()
--- index.py
+++ index.py
@@ -5,55 +5,23 @@ Created on Fri Mar  2 23:20:35 2018
 @author: Mathu_Gopalan
 """
 
-import util,helpers,time,pyautogui,re
-from selenium import webdriver
+import helpers
 import pandas as pd
 
 
 def initilize(file):
     global configs,df    
-    configs = helpers.ReadConfig(file)
+    configs = helpers.readconfig(file)
     print(configs)
     df = pd.read_csv(configs["InputFile"]) 
-    helpers.createFolders(configs) 
+    helpers.createfolders(configs) 
     return True
 
-def gen_ScreenSnap(method=None):
-    options = webdriver.ChromeOptions()
-    options.add_argument('--load-extension='+configs["ExtensionPath"])
-    # Navigate to any page... well, not just *any* page...
-    driver = webdriver.Chrome(chrome_options=options)
-    driver.maximize_window()
-    driver.get("https://www.google.com")
-    time.sleep(2)
-    i=0
-    if (method=="wireframe"):
-        v=pyautogui.locateOnScreen("findicon.PNG")
-        folder_loc = configs["wfImages"]
-    else : folder_loc = configs["Rawimages"]
-    
-    for url,b_name in zip(df["url_name"],df["brand_name"]):
-        driver.get(url.strip())
-        time.sleep(2)
-        pg_title = driver.title
-        pg_title = helpers.slugify(pg_title)
-        #pg_title = re.sub('[^a-zA-Z0-9-_!@#©|?|$|.|!*.]', '',pg_title)
-        pg_title = b_name+"_"+str(i)+ "_"+ pg_title
-        if len(pg_title)>240:
-            pg_title = pg_title[0:239]
-        print(pg_title)
-        if (method =="wireframe"):
-             #pyautogui to click on the extension
-            pyautogui.click(x=v[0],y=v[1],clicks=1,interval=0.0,button="left")
-            time.sleep(5)
-        util.fullpage_screenshot(driver,folder_loc,pg_title)
-        i+=1
-    driver.close()
 
 
 initilize("config.yml")
-gen_ScreenSnap()
-gen_ScreenSnap("wireframe")
+helpers.gen_screensnap(df, configs)
+helpers.gen_screensnap(df, configs,"wireframe")
 helpers.createcanny(configs["wfImages"],configs["cannyImg"])
 
 
